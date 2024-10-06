0e9cc5c6dbb9965416063c5a42142a1e065e7d48
teslakitty <teslakitty18@gmail.com>
2024-10-06 15:01:01 -0400

    file changes and new files
    removed license
    readme.md updated
    added index.html style.css and script.js

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # kiosk-page
-kiosk page for kiosk devices you may fork it for editing the page
+## kiosk page for kiosk devices you may fork it for editing the page to the way you want
---
+++ src/script.js
@@ -0,0 +1,7 @@
+// Function to open all the links
+document.getElementById('openAllBtn').addEventListener('click', function() {
+    // Open each URL in a new tab
+    window.open('http://ha:8123', '_blank');
+    window.open('http://mediapi.local', '_blank');
+    window.open('https://cashier2.netlify.app', '_blank');
+});
